30aa4f4a76f25f0234b57f51c91e9921a43361c8
Tzu-Yu, Chen <s107062562@m107.nthu.edu.tw>
2020-11-21 21:10:31 +0800

    feat(frame_visulizer): generate the attributes of the three types of UEs

--- utils/frame_visualizer/frame_visualizer.py
+++ utils/frame_visualizer/frame_visualizer.py
@@ -4,6 +4,9 @@ from typing import List
 from src.resource_allocation.ds.eutran import EUserEquipment
 from src.resource_allocation.ds.frame import Frame
 from src.resource_allocation.ds.ngran import DUserEquipment, GUserEquipment
+from src.resource_allocation.ds.rb import ResourceBlock
+from src.resource_allocation.ds.ue import UserEquipment
+from src.resource_allocation.ds.util_enum import UEType
 
 
 class FrameRenderer:
@@ -24,7 +27,33 @@ class FrameRenderer:
         ]
         self.body = []
 
-    def gen_tables(self, frame: Frame):
+    def generate_rb(self, rb_list: List[ResourceBlock]):
+        rb_list = sorted(rb_list, key=lambda x: x.position[0])
+        rb_i: int = 0
+        for rb in rb_list:
+            if rb_i != rb.position[0]:
+                self.body.append('<br>')
+                rb_i: int = rb.position[0]
+            self.body.append(f'[{rb.position[0]}, {rb.position[2]}]')
+
+    def generate_ue_list(self, ue_list: List[UserEquipment]):
+        self.body.append(f'\n<div><p>{ue_list[0].ue_type.name}UE:</p>')
+        for ue in ue_list:
+            self.body.append(f'\n<div><b>User ID</b>: {ue.uuid.hex[:4]}\n<div>')
+            self.body.append(f'\nnumerology: {ue.numerology_in_use}')
+            self.body.append(f'\nQos: {ue.request_data_rate}')
+            if ue.ue_type == UEType.G or ue.ue_type == UEType.D:
+                self.body.append(f'\n<br>gnb_info: MCS: {ue.gnb_info.mcs.name}')
+                self.body.append(f'\nthe number of RBs: {len(ue.gnb_info.rb)}')
+                self.generate_rb(ue.gnb_info.rb)
+            if ue.ue_type == UEType.E or ue.ue_type == UEType.D:
+                self.body.append(f'\n<br>enb_info: MCS: {ue.enb_info.mcs.name}')
+                self.body.append(f'\nthe number of RBs: {len(ue.enb_info.rb)}')
+                self.generate_rb(ue.enb_info.rb)
+            self.body.append('\n</div>\n</div>')
+        self.body.append('\n</div>')
+
+    def gen_layer(self, frame: Frame):
         for layer in frame.layer:
             base_unit = layer.bu
             height, width = len(base_unit), len(base_unit[0])
@@ -56,7 +85,7 @@ class FrameRenderer:
 
 
 if __name__ == '__main__':
-    file_to_visualize = "vis_20201121.P"
+    file_to_visualize = "vis_20201121"
     gFrame: List[Frame] = []
     eFrame: List[Frame] = []
     g_ue_list: List[List[GUserEquipment]] = []
@@ -76,6 +105,9 @@ if __name__ == '__main__':
                 break
 
     frame_renderer = FrameRenderer()
-    frame_renderer.gen_tables(gFrame[0])
-    frame_renderer.gen_tables(eFrame[0])
+    frame_renderer.gen_layer(gFrame[0])
+    frame_renderer.gen_layer(eFrame[0])
+    frame_renderer.generate_ue_list(g_ue_list[0])
+    frame_renderer.generate_ue_list(d_ue_list[0])
+    frame_renderer.generate_ue_list(e_ue_list[0])
     frame_renderer.render(file_to_visualize+'.html')
