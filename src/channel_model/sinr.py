1449d0b3afabf051ff3154054cee60e411395d6d
Tzu-Yu, Chen <s107062562@m107.nthu.edu.tw>
2020-12-01 22:46:54 +0800

    fix(sinr): The unit of the channel models are a mess!
    * Rewrite rx power calulation to tx power - path loss - shadow.
      Instead of path loss to channel gain then times tx power.
    * Times AWGN with bandwidth.
    * Save SINR in dB.
    * Fix the error of seeing tx power as mW instead of dBm.

--- src/channel_model/sinr.py
+++ src/channel_model/sinr.py
@@ -24,6 +24,11 @@ class ChannelModel:
         rb.sinr = tmp_sinr_rb
 
     def sinr_bu(self, bu: BaseUnit):
+        """
+        SINR(ratio) = rx power(mW) / (interference_noma(mW) + interference_ini(mW) + interference_cross(mW) + awgn(mW))
+        :param bu: The BU to calculate its' SINR.
+        :return: SINR in dB
+        """
         rb: ResourceBlock = bu.within_rb
         ue: UserEquipment = rb.ue
         nodeb: NodeB = bu.layer.nodeb
@@ -35,18 +40,19 @@ class ChannelModel:
         interference_cross: float = 0.0
         for layer in nodeb.frame.layer:
             if layer != bu.layer:  # only for gNB, which has multiple layers
-                print(f'layer: {layer.nodeb.nb_type} {layer.layer_index}')
+                print(f'{bu.relative_j}layer: {layer.nodeb.nb_type} {layer.layer_index}')
                 overlapped_bu: BaseUnit = layer.bu[bu.absolute_i][bu.absolute_j]
                 if not (overlapped_rb := overlapped_bu.within_rb):  # if the radio resource is not allocated
                     continue
                 overlapped_bu_power_rx: float = self.power_rx(overlapped_rb.ue.gnb_info.distance,
                                                               nodeb.power_tx)  # nodeb.power_tx = layer.nodeb.power_tx = overlapped_rb.ue.gnb_info.nb.power_tx
-                if overlapped_bu_power_rx > power_rx:  # TODO: should be comparing channel gain. However, the rx power of the two UE are the same.
+                print(f'{bu.relative_j}power rx: {power_rx}')
+                if overlapped_bu_power_rx > power_rx:  # should be comparing channel gain. However, the rx power of the two UE are the same.
                     interference_noma += overlapped_bu_power_rx
-                    print(f'interference_noma: {interference_noma}')
+                    print(f'{bu.relative_j}interference_noma: {interference_noma}')
                 if rb.numerology != overlapped_rb.numerology:
                     interference_ini += overlapped_bu_power_rx
-                    print(f'interference_ini: {interference_ini}')
+                    print(f'{bu.relative_j}interference_ini: {interference_ini}')
         if bu.is_cochannel:
             for layer in bu.cochannel_nb.frame.layer:
                 print(f'layer: {layer.nodeb.nb_type} {layer.layer_index}')
@@ -62,25 +68,19 @@ class ChannelModel:
                     interference_ini += overlapped_bu_power_rx
                     print(f'interference_ini(cross): {interference_ini}')
 
-        awgn_noise: float = pow(10, 17.4) * 180_000  # awgn noise: mW, awgn: mW/Hz, bu_bandwidth: 180_000 Hz
-        bu.sinr = power_rx / (interference_noma + interference_ini + interference_cross + awgn_noise)
+        sinr = power_rx / (interference_noma + interference_ini + interference_cross + self.awgn_noise)  # ratio
+        bu.sinr = 10 * math.log10(sinr)  # ratio to dB
 
     def power_rx(self, distance: float, power_tx: float) -> float:
         """
         Calculate the degraded signal transmitted by the BS to the UE.
+        rx power(dBm) = tx power(dBm) - path loss(dB) - shadowing(dB)
         :param distance: in km. The distance from the BS to the UE.
-        :param power_tx: in mW. The transmit power from BS to UE.
+        :param power_tx: in dBm. The transmit power from BS to UE.
         :return power_rx: in mW. The receive power from BS of UE.
         """
-        return self.channel_gain(distance) * power_tx
-
-    def channel_gain(self, distance: float) -> float:
-        """
-        Calculate the channel gain of a UE.
-        Channel gain = path loss * shadowing
-        :return channel gain: in ratio
-        """
-        return self._path_loss(distance) * self.noise(8)  # TODO: 公式對嗎？path loss直接是channel gain?
+        power_rx: float = power_tx - self._path_loss(distance) - self.noise(8)  # dBm
+        return pow(10, power_rx / 10)  # dBm to mW
 
     @staticmethod
     def _path_loss(distance: float) -> float:
@@ -88,10 +88,20 @@ class ChannelModel:
         A UMa (urban macro-cell) outdoor path loss model.
         ref: TR 36.931 v13.0.0
         :param distance: in kilometer
-        :return path loss: in ratio
+        :return path loss: in dB
         """
-        path_loss = 128.1 + 37.6 * math.log10(distance)  # dB
-        return pow(10, path_loss / 10)  # dB to ratio
+        return 128.1 + 37.6 * math.log10(distance)  # dB
+
+    @property
+    def awgn_noise(self) -> float:
+        """
+        AWGN times bandwidth, reference by Techplayon, Signal to Interference and Noise Ratio (SINR),
+        http://www.techplayon.com/signal-to-interference-and-noise-ratio-snir/?fbclid=IwAR3cvJQAzcDfA4o1u5zEtbO_Q-ADdPm6wxUS6-dBA7VYjMBNVJLcegTOrnE
+        """
+        power_spectral_density: int = -174  # dBm/Hz
+        bandwidth: int = 180_000  # Hz, for a BU
+        noise_power: float = power_spectral_density + 10 * math.log10(bandwidth)  # dBm
+        return pow(10, noise_power / 10)  # dBm to mW
 
     def noise(self, noise_variance: int) -> float:
         seed: int = 0 - randint(1, 100)
@@ -107,7 +117,7 @@ class ChannelModel:
         log_value: float = math.log10(slevel)
         stemp: float = 2 * log_value / slevel  # original -2
         noise: float = math.sqrt(noise_variance) * runiform[0] * math.sqrt(stemp)
-        return noise
+        return noise  # dB
 
     @staticmethod
     def bsd_rand(seed: int) -> float:
@@ -124,15 +134,21 @@ if __name__ == '__main__':
     layer_: Layer = gNB.frame.layer[1]
     ue_: UserEquipment = DUserEquipment(12345, [Numerology.N1])
     ue_.set_numerology(Numerology.N1)
-    ue_.gnb_info.distance = 0.9
+    ue_.gnb_info.distance = 0.000000000001
     rb_: ResourceBlock = ResourceBlock(layer_, 2, 2, ue_)
     layer_.allocate_resource_block(2, 2, ue_)
 
-    layer__: Layer = gNB.frame.layer[0]
-    ue__: UserEquipment = DUserEquipment(12345, [Numerology.N1])
-    ue__.set_numerology(Numerology.N1)
-    ue__.gnb_info.distance = 0.1
-    rb__: ResourceBlock = ResourceBlock(layer__, 2, 2, ue__)
-    layer__.allocate_resource_block(2, 2, ue__)
+    layer_2: Layer = gNB.frame.layer[0]
+    ue_2: UserEquipment = DUserEquipment(12345, [Numerology.N1, Numerology.N2])
+    ue_2.set_numerology(Numerology.N2)
+    ue_2.gnb_info.distance = 0.000000000001
+    rb_2: ResourceBlock = ResourceBlock(layer_2, 2, 2, ue_2)
+    layer_2.allocate_resource_block(2, 2, ue_2)
+
+    ue_3: UserEquipment = DUserEquipment(12345, [Numerology.N1, Numerology.N2])
+    ue_3.set_numerology(Numerology.N2)
+    ue_3.gnb_info.distance = 1
+    rb_3: ResourceBlock = ResourceBlock(layer_2, 2, 6, ue_3)
+    layer_2.allocate_resource_block(2, 6, ue_3)
 
     ChannelModel().sinr_rb(rb_)
