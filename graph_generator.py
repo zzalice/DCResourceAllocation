1675e07192fd1f903a69f4872b8786ae9d495cdb
Tzu-Yu, Chen <s107062562@m107.nthu.edu.tw>
2021-03-06 21:40:14 +0800

    feat(timer): Print execution time in graph_generator.

--- graph_generator.py
+++ graph_generator.py
@@ -7,9 +7,10 @@ from src.resource_allocation.algo.utils import bpframe_to_mbps, calc_system_thro
 from src.resource_allocation.ds.eutran import ENodeB, EUserEquipment
 from src.resource_allocation.ds.ngran import DUserEquipment, GNodeB, GUserEquipment
 from src.simulation.data.util_graph import line_chart
+import time
 
 if __name__ == '__main__':
-    time: int = 2
+    times: int = 2
     max_layers: List[int] = [1]
     file_path: str = 'hotspot'
 
@@ -20,10 +21,14 @@ if __name__ == '__main__':
     for l in range(len(max_layers)):
         print(f'l: {max_layers[l]}')
         data_set_file_path: str = f'{file_path}/{max_layers[l]}layer/'
-        for i in range(time):
+        for i in range(times):
             print(f'i:{i}')
+            start_time = time.time()
             result['DC-RA'].append(dc_resource_allocation(data_set_file_path + str(i)))
+            print("--- %s sec DC-RA ---" % round((time.time() - start_time), 3))
+            start_time = time.time()
             result['Intuitive'].append(intuitive_resource_allocation(data_set_file_path + str(i)))
+            print("--- %s sec Intui ---" % round((time.time() - start_time), 3))
 
             # sum system throughput
             for data in avg_system_throughput:
@@ -32,12 +37,12 @@ if __name__ == '__main__':
 
         # avg system throughput
         for data in avg_system_throughput:
-            avg_system_throughput[data][l] /= time
+            avg_system_throughput[data][l] /= times
             avg_system_throughput[data][l] = bpframe_to_mbps(avg_system_throughput[data][l],
                                                              result[data][-1][0].frame.frame_time)
 
     line_chart('', 'The number of gNB layer', max_layers, 'System throughput(Mbps)', avg_system_throughput,
-               f'src/simulation/data/{file_path}/sys_rate_{str(time)}time')
+               f'src/simulation/data/{file_path}/sys_rate_{str(times)}time')
 
-    with open(f'src/simulation/data/{file_path}/graph_generator_{str(time)}time.P', 'wb') as f:
+    with open(f'src/simulation/data/{file_path}/graph_generator_{str(times)}time.P', 'wb') as f:
         pickle.dump(result, f)
