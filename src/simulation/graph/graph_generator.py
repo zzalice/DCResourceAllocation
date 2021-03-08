6b94760b4007a64894f4fef5ce0c6b2aa17f98e8
Tzu-Yu, Chen <s107062562@m107.nthu.edu.tw>
2021-03-08 16:41:29 +0800

    fix: Out of memory. BREAKING CHANGE: In graph generator, save NB and UEs dynamically.

--- src/simulation/graph/graph_generator.py
+++ src/simulation/graph/graph_generator.py
@@ -20,49 +20,57 @@ class GraphGenerator:
         if not os.path.exists(self.output_file_path):
             os.makedirs(self.output_file_path)
 
-        self.result: Dict[
-            str, List[Tuple[GNodeB, ENodeB, List[DUserEquipment], List[GUserEquipment], List[EUserEquipment]]]] = {
-            'DC-RA': [], 'Intuitive': []}
-
     def gen_sys_throughput_layer(self, layers: List[int]):
+        pickle_file_path: str = f'{self.output_file_path}/dcra_intuitive.P'
+        with open(pickle_file_path, 'wb') as f:
+            pickle.dump('new file', f)
+        program_start_time = time.time()
+
         avg_system_throughput: Dict[str, List[Any]] = {'DC-RA': [0.0 for _ in range(len(layers))],
                                                        'Intuitive': [0.0 for _ in range(len(layers))]}
-        program_start_time = time.time()
         for i in range(len(layers)):
             print(f'l: {layers[i]}')
-            self._run_algo(f'{self.folder}/{layers[i]}layer/')
+            data_description: str = f'{layers[i]}layer'
+            result: Dict[str, Dict[str, List[Tuple[GNodeB, ENodeB, List[DUserEquipment], List[GUserEquipment], List[
+                EUserEquipment]]]]
+                ] = self._run_algo(f'{self.folder}/{layers[i]}layer/', pickle_file_path, data_description)
 
             # sum system throughput
             for j in range(self.times):
                 for data in avg_system_throughput:
                     avg_system_throughput[data][i] += calc_system_throughput_uncategorized_ue(
-                        self.result[data][j][2] + self.result[data][j][3] + self.result[data][j][4])
+                        result[data_description][data][j][2] + result[data_description][data][j][3] +
+                        result[data_description][data][j][4])
 
             # avg system throughput
             for data in avg_system_throughput:
                 avg_system_throughput[data][i] /= self.times
                 avg_system_throughput[data][i] = bpframe_to_mbps(avg_system_throughput[data][i],
-                                                                 self.result[data][-1][0].frame.frame_time)
+                                                                 result[data_description][data][-1][0].frame.frame_time)
 
         line_chart('', 'The number of gNB layer', ([str(i) for i in layers]), 'System throughput(Mbps)',
                    avg_system_throughput, self.output_file_path, self._parameter())
 
-        with open(f'{self.output_file_path}/dcra_intuitive.P', 'wb') as f:
-            pickle.dump(self.result, f)
-
         print("--- %s sec ---" % round((time.time() - program_start_time), 3))
 
-    def _run_algo(self, data_set_file_path: str):
+    def _run_algo(self, data_set_file_path: str, pickle_file_path: str, result_information: str):
+        result: Dict[str, Dict[
+            str, List[Tuple[GNodeB, ENodeB, List[DUserEquipment], List[GUserEquipment], List[EUserEquipment]]]]] = {
+            result_information: {'DC-RA': [], 'Intuitive': []}}
         for i in range(self.times):
             print(f'i:{i}')
 
             start_time = time.time()
-            self.result['DC-RA'].append(dc_resource_allocation(data_set_file_path + str(i)))
+            result[result_information]['DC-RA'].append(dc_resource_allocation(data_set_file_path + str(i)))
             print("--- %s sec DC-RA ---" % round((time.time() - start_time), 3))
 
             start_time = time.time()
-            self.result['Intuitive'].append(intuitive_resource_allocation(data_set_file_path + str(i)))
+            result[result_information]['Intuitive'].append(intuitive_resource_allocation(data_set_file_path + str(i)))
             print("--- %s sec Intui ---" % round((time.time() - start_time), 3))
+
+        with open(pickle_file_path, 'ab+') as f:
+            pickle.dump(result, f)
+        return result
 
     def _parameter(self) -> Dict:
         return {'times': self.times}
