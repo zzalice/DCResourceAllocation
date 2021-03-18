e9cc31825dfc00023c09a3ea1eaa2c714919a43e
Tzu-Yu, Chen <s107062562@m107.nthu.edu.tw>
2021-03-18 15:00:25 +0800

    refactor(main_gen_graph): Let parameter changing simpler.

--- main_gen_graph.py
+++ main_gen_graph.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from src.simulation.graph.graph_generator import GraphGenerator
 from src.simulation.iteration import IterateAlgo
 
@@ -6,16 +8,14 @@ if __name__ == '__main__':
     MCS parameter can be set in resource_allocation/ds/util_enum.py
     and record in IterateAlgo output pickle.
     """
-    IterateAlgo().iter_layer(
-        folder_data='0317-100632small_frame50_moreUE',
-        iteration=10, layers=[1, 2, 3, 4, 5])
+    f_data: str = '0317-100632small_frame50_moreUE'
+    f_mcs: str = 'gNBCQI1CQI15_eNBCQI1CQI15'
+    i: int = 10
+    l: List[int] = [1, 2, 3, 4, 5]
+    IterateAlgo().iter_layer(iteration=i, layers=l, folder_data=f_data)
 
-    GraphGenerator(
-        folder_result=('0317-100632small_frame50_moreUE/gNBCQI1CQI15_eNBCQI1CQI15',),
-        iteration=10, layers=[1, 2, 3, 4, 5], graph_type='sys throughput - layer')
-    GraphGenerator(
-        folder_result=('0317-100632small_frame50_moreUE/gNBCQI1CQI15_eNBCQI1CQI15',),
-        iteration=10, layers=[1, 2, 3, 4, 5], graph_type='used percentage')
+    GraphGenerator(iteration=i, layers=l, graph_type='sys throughput - layer', folder_result=(f'{f_data}/{f_mcs}',))
+    GraphGenerator(iteration=i, layers=l, graph_type='used percentage', folder_result=(f'{f_data}/{f_mcs}',))
     # GraphGenerator(times=10, folder='standard').gen_deployment('/Users/hscc/Downloads/Papers/simulation/standard/dcra_intuitive.P')
     # GraphGenerator(times=10, folder=['standard', 'radius_05km', 'large_radius']).gen_avg_allocated_ue(
     #     ['/Users/hscc/Downloads/Papers/simulation/', '/dcra_intuitive.P'])
