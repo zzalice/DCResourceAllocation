4d1fe873955c8c56aa47f48beab25f9d47fb9d19
Tzu-Yu, Chen <s107062562@m107.nthu.edu.tw>
2021-04-17 14:39:24 +0800

    feat(gen data): Split layer and ue generator into two files.

--- main_gen_data_layer.py
+++ main_gen_data_layer.py
@@ -1,11 +1,10 @@
-import math
 from datetime import datetime
-from typing import Dict, List
+from typing import Dict
 
 from src.simulation.data.data_generator import DataGenerator
 
 
-def main(parameter: Dict):
+def main_gen_data(parameter: Dict):
     DataGenerator(iteration=parameter['iteration'], output_file_path=parameter['output_file_path'],
                   eue_num=parameter['eue_num'], eue_qos_range=parameter['eue_qos'], eue_hotspots=parameter['eue_hotspots'],
                   gue_num=parameter['gue_num'], gue_qos_range=parameter['gue_qos'], gue_hotspots=parameter['gue_hotspots'],
@@ -21,58 +20,29 @@ def main(parameter: Dict):
                   worsen_threshold=parameter['worsen_threshold']).generate_data()
 
 
-def calc_num_ue(total: List[int], proportion) -> List[Dict[str, int]]:
-    """
-    With given number of total UE in the whole system.
-    Calculate the number of UE of each type by the input proportion.
-    :param total: A list of total UE.
-    :param proportion: The proportion of the num of dUE, gUE, and eUE.
-    :return: [{'total': int, 'num_due': int, 'num_gue': int, 'num_eue': int}, {...}, ...]
-    """
-    p_total = 0
-    for p in proportion:
-        p_total += p
-    result: List[Dict[str, int]] = []
-    for t in total:
-        due_num = math.ceil(t * (proportion[0] / p_total))
-        gue_num = math.ceil(t * (proportion[1] / p_total))
-        eue_num = math.ceil(t * (proportion[2] / p_total))
-        result.append({'total': t, 'due': due_num, 'gue': gue_num, 'eue': eue_num})
-    return result
-
-
 if __name__ == '__main__':
     date: str = datetime.today().strftime("%m%d-%H%M%S")
     output_folder: str = f'{date}avg_deploy'  # <--- change
 
-    # # --- for layer ---
-    # num_of_layer = [1, 2, 3, 4, 5]  # <--- change
-    # for i in num_of_layer:
-    #     para = {'output_file_path': f'{output_folder}/{i}layer',
-    # # --- for ue ---
-    # # due:gue:eue_num = 3:4:17   when radius 0.5 and 0.3, coordinate gNB (0.5, 0)
-    # # due:gue:eue_num = 2:3:6    when radius 0.5 and 0.4, coordinate gNB (0.5, 0)
-    # # due:gue:eue_num = 29:21:49    when radius 0.5 and 0.4, coordinate gNB (0.4, 0)
-    num_of_total_ue = [300, 400, 500, 600, 700, 800, 900]  # <--- change
-    proportion_of_ue = [2, 3, 6]  # <--- change
-    num_of_ue = calc_num_ue(num_of_total_ue, proportion_of_ue)
-    for i in num_of_ue:
-        para = {'output_file_path': f'{output_folder}/{i["total"]}ue',
+    # --- for layer ---
+    num_of_layer = [1, 2, 3, 4, 5]  # <--- change
+    for i in num_of_layer:
+        para = {'output_file_path': f'{output_folder}/{i}layer',
                 'iteration': 100,
-                'due_num': i['due'],
+                'due_num': 100,
                 'due_qos': [16_000, 100_000],
                 'due_hotspots': (),  # e.g. ((-0.15, 0.0, 0.15, 75),) => (x, y, radius, #ue)
-                'gue_num': i['gue'],
+                'gue_num': 150,
                 'gue_qos': [16_000, 100_000],
                 'gue_hotspots': (),
-                'eue_num': i['eue'],
+                'eue_num': 300,
                 'eue_qos': [16_000, 100_000],
                 'eue_hotspots': (),
 
                 # gnb_freq(MHz/#): 5/25, 10/52, 15/79, 20/106, 25/133, 30/160, 40/216, 50/270, 60/324, 70/378, 80/434, 90/490, 100/546
                 # enb_freq(MHz/#): 1.4/6, 3/15, 5/25, 10/50, 15/75, 20/100
                 'gnb_freq': 25, 'gnb_time': 8, 'gnb_radius': 0.4, 'gnb_coordinate': (0.5, 0.0), 'gnb_tx_power': 30,
-                'gnb_layer': 5, 'inr_discount': 0.5,
+                'gnb_layer': i, 'inr_discount': 0.5,
                 'enb_freq': 200, 'enb_time': 8, 'enb_radius': 0.5, 'enb_coordinate': (0.0, 0.0), 'enb_tx_power': 46,
 
                 'cochannel_bandwidth': 0,
@@ -80,4 +50,4 @@ if __name__ == '__main__':
                 # range of MCS: in file resource_allocation/ds/util_enum.py
                 }
 
-        main(para)
+        main_gen_data(para)
---
+++ main_gen_data_ue.py
@@ -0,0 +1,69 @@
+import math
+from datetime import datetime
+from typing import Any, Dict, List
+
+from main_gen_data_layer import main_gen_data
+
+
+def calc_num_ue(total: List[int], proportion) -> List[Dict[str, int]]:
+    """
+    With given number of total UE in the whole system.
+    Calculate the number of UE of each type by the input proportion.
+    :param total: A list of total UE.
+    :param proportion: The proportion of the num of dUE, gUE, and eUE.
+    :return: [{'total': int, 'num_due': int, 'num_gue': int, 'num_eue': int}, {...}, ...]
+    """
+    p_total = 0
+    for p in proportion:
+        p_total += p
+    result: List[Dict[str, int]] = []
+    for t in total:
+        due_num = math.ceil(t * (proportion[0] / p_total))
+        gue_num = math.ceil(t * (proportion[1] / p_total))
+        eue_num = math.ceil(t * (proportion[2] / p_total))
+        result.append({'total': t, 'due': due_num, 'gue': gue_num, 'eue': eue_num})
+    return result
+
+
+def gen_data_number_ue(num_of_total_ue: List[int], proportion_of_ue: List[int],
+                       parameter: Dict[str, Any], folder: str):
+    # due:gue:eue_num = 3:4:17   when radius 0.5 and 0.3, coordinate gNB (0.5, 0)
+    # due:gue:eue_num = 2:3:6    when radius 0.5 and 0.4, coordinate gNB (0.5, 0)
+    # due:gue:eue_num = 29:21:49    when radius 0.5 and 0.4, coordinate gNB (0.4, 0)
+    num_of_ue = calc_num_ue(num_of_total_ue, proportion_of_ue)
+    for i in num_of_ue:
+        parameter['output_file_path'] = f'{folder}/{i["total"]}ue'
+        parameter['due_num'] = i['due']
+        parameter['gue_num'] = i['gue']
+        parameter['eue_num'] = i['eue']
+
+        main_gen_data(parameter)
+
+
+if __name__ == '__main__':
+    date: str = datetime.today().strftime("%m%d-%H%M%S")
+    output_folder: str = f'{date}avg_deploy'  # <--- change
+    para = {'iteration': 100,
+            'due_qos': [16_000, 100_000],
+            'due_hotspots': (),  # e.g. ((-0.15, 0.0, 0.15, 75),) => (x, y, radius, #ue)
+            'gue_qos': [16_000, 100_000],
+            'gue_hotspots': (),
+            'eue_qos': [16_000, 100_000],
+            'eue_hotspots': (),
+
+            # gnb_freq(MHz/#): 5/25, 10/52, 15/79, 20/106, 25/133, 30/160, 40/216, 50/270, 60/324, 70/378, 80/434, 90/490, 100/546
+            # enb_freq(MHz/#): 1.4/6, 3/15, 5/25, 10/50, 15/75, 20/100
+            'gnb_freq': 25, 'gnb_time': 8, 'gnb_radius': 0.4, 'gnb_coordinate': (0.5, 0.0), 'gnb_tx_power': 30,
+            'gnb_layer': 5, 'inr_discount': 0.5,
+            'enb_freq': 200, 'enb_time': 8, 'enb_radius': 0.5, 'enb_coordinate': (0.0, 0.0), 'enb_tx_power': 46,
+
+            'cochannel_bandwidth': 0,
+            'worsen_threshold': -100_000_000  # bps
+            # range of MCS: in file resource_allocation/ds/util_enum.py
+            }
+
+    # due:gue:eue_num = 3:4:17   when radius 0.5 and 0.3, coordinate gNB (0.5, 0)
+    # due:gue:eue_num = 2:3:6    when radius 0.5 and 0.4, coordinate gNB (0.5, 0)
+    # due:gue:eue_num = 29:21:49    when radius 0.5 and 0.4, coordinate gNB (0.4, 0)
+    gen_data_number_ue(num_of_total_ue=[300, 400, 500, 600, 700, 800, 900],
+                       proportion_of_ue=[2, 3, 6], parameter=para, folder=output_folder)
