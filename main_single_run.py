09aa5d38bf6b5d3bb3fe6b8541f147d535f68e85
Tzu-Yu, Chen <s107062562@m107.nthu.edu.tw>
2021-05-18 09:15:54 +0800

    feat(run one iter): Specify certain iteration of algorithm to run.

---
+++ main_single_run.py
@@ -0,0 +1,8 @@
+from src.simulation.iteration import OneIterationAlgo
+
+if __name__ == '__main__':
+    folder_data: str = '0513-010046L_'
+    folder_topic: str = '3layer'
+    file_iter: int = 33
+    algorithm: str = 'DC-RA'  # 'DC-RA', 'FRSA', 'MSEMA', 'Baseline'
+    OneIterationAlgo(folder_data, folder_topic, file_iter, algorithm)
