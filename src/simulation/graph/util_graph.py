67265805913fdde43c08a7800e98cb8bbd654672
Tzu-Yu, Chen <s107062562@m107.nthu.edu.tw>
2021-06-08 16:38:25 +0800

    feat(util_graph): Redraw a graph by input a json file.

--- src/simulation/graph/util_graph.py
+++ src/simulation/graph/util_graph.py
@@ -211,38 +211,10 @@ def dump_json(path: str, data: Any):
 
 
 if __name__ == '__main__':
-    # line chart
-    # line_chart("Line chart",
-    #            "month", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-    #            "price", {"TSMC": [255, 246, 247.5, 227, 224, 216.5, 246, 256, 262.5, 234, 225.5, 225.5],
-    #                      "FOX": [92.2, 88.1, 88.5, 82.9, 85.7, 83.2, 83.8, 80.5, 79.2, 78.8, 71.9, 70.8]}, 'line_chart')
-    #
-    # with open('line_chart.json') as fp:
-    #     data = json.load(fp)
-    #     line_chart(data[0], data[1], data[2], data[3], data[4], data[5])
-
-    # bar chart
-    # labels = ['G1', 'G2', 'G3', 'G4', 'G5']
-    # raw_data = {'men': [20, 34, 30, 35, 27], 'women': [25, 32, 34, 20, 25], 'child': [1, 2, 3, 4, 5]}
-    # bar_chart(title='Scores by group and gender', x_tick_labels=labels, y_label='Scores', data=raw_data)
-
-    # bar chart grouped and stacked
-    # import pandas as pd
-    # import numpy as np
-    # import matplotlib.pyplot as plt
-    # df1 = pd.DataFrame(np.random.rand(4, 5),
-    #                    index=["A", "B", "C", "D"],
-    #                    columns=["I", "J", "K", "L", "M"])
-    # df2 = pd.DataFrame(np.random.rand(4, 5),
-    #                    index=["A", "B", "C", "D"],
-    #                    columns=["I", "J", "K", "L", "M"])
-    # df3 = pd.DataFrame(np.random.rand(4, 5),
-    #                    index=["A", "B", "C", "D"],
-    #                    columns=["I", "J", "K", "L", "M"])
-    # bar_chart_grouped_stacked([df1, df2, df3], ["df1", "df2", "df3"])
-
-    ##########################################
-    file_path: str = '0410-094738high_qos_700ue/gNBCQI1CQI7_eNBCQI1CQI7/num_of_allocated_ue_0410-1417.json'
-    with open(file_path, 'r') as f:
+    folder_output: str = '0603-125541P_DUE_5mhz_qos300/gNBCQI1CQI5_eNBCQI1CQI5/'
+    file_name: str = "The Proportion of dUE_Jain's Fairness Index_0607-1253拷貝.json"
+    with open(f'{folder_output}{file_name}', 'r') as f:
         d = json.load(f)
-        bar_chart_grouped_stacked(d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8])
+        line_chart(d[0], d[1], d[2], d[3], d[4], folder_output, d[6])
+        # bar_chart(d[0], d[1], d[2], d[3], d[4], folder_output, d[6])
+        # bar_chart_grouped_stacked(d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8])
